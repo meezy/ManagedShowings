007b65de2e966f3931b81ca20cde60d895255be7
jmarcum <jmarcum@gmail.com>
2012-07-05 20:08:06 -0700

    AppDrone: ManagedShowings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManagedShowings::Application.config.secret_token = '7fd27c91202cda5a399f9ccc28773dbe9dce4d0a3a1f9d3f5c8a85079246ef611c8cb0b59ad4a0c419cc3a0cd344e37f53c606ac3ffe3d43bcac69d020ae8cbf'
